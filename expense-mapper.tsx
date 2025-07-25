fdf02eea8c01a39cf3f31e177814ec176b83fa5e
Avinava Basu <avinava@mycelium.ventures>
2025-07-26 00:17:00 +0530

    Security cleanup: Remove unnecessary legacy file
    - Remove outdated expense-mapper.tsx that was just a redirect
    - Clean up project structure for GitHub repository

--- expense-mapper.tsx
+++
@@ -1,5 +0,0 @@
-// This file has been refactored into a modular structure
-// The main component is now in src/components/ExpenseMapper.tsx
-// Run `npm install` to install dependencies, then `npm run dev` to start the development server
-
-export { default } from './src/components/ExpenseMapper';
